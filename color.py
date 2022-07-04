2e02f7b120fd8f014c82e881f1327ce83f0209cf
Aidan Andersen <aidan@andersenweb.com>
2022-07-04 17:28:34 -0600

    Added list to color for menu functionality

--- color.py
+++ color.py
@@ -43,6 +43,18 @@ def yWork(y):
     else:
         return y
 
+def text_objects(text,font):
+    textSurface = font.render(text, True, white)
+    return textSurface,textSurface.get_rect()
+
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf',115)
+    TextSurf, TextRect = text_objects(text,largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+    pygame.display.update()
+  #  time.sleep(1)
+
 def game_loop():
     gameDisplay.fill(black)
   #  print("Hello")
@@ -73,14 +85,9 @@ def game_loop():
     check = 0
     while True:
         
+        gameDisplay.fill(black)
+       # randcolor = (random.randint(0,255),random.randint(0,255),random.randint(0,255)) 
         
-        randcolor = (random.randint(0,255),random.randint(0,255),random.randint(0,255)) 
-        block(x6, y6, dotS, dotS, white)
-        block(x1, y1, dotS, dotS, white)
-        block(x2, y2, dotS, dotS, white)
-        block(x3, y3, dotS, dotS, white)
-        block(x4, y4, dotS, dotS, white)
-        block(x5, y5, dotS, dotS, white)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -88,6 +95,8 @@ def game_loop():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     print("hello")
+                    message_display("Level UP")
+                    #time.sleep(3)
                 #elif event.key == pygame.K_RIGHT:
               #      x_change = 5
             if event.type == pygame.KEYUP:
@@ -95,42 +104,72 @@ def game_loop():
                     x_change = 0
                     print("Never let it go")
         
+        
+        #Updating stuff
+        
+        for i in range((int)(display_width/dotS)):
+             #   for z in range[((int)(display_height/dotS))]:
+                for z in range((int)(display_height/dotS)):  
+                   # print("Hello thereafdadsfasdf")
+                    test = arr[i][z]
+                    if test == 'red':
+                        block(i*dotS, z*dotS, dotS, dotS, (255, 0, 0))
+                    elif test == 'orange':
+                        block(i*dotS, z*dotS, dotS, dotS, (255, 120, 0))
+                    elif test == 'yellow':
+                        block(i*dotS, z*dotS, dotS, dotS, (255, 255, 0))
+                    elif test == 'green':
+                        block(i*dotS, z*dotS, dotS, dotS, (0, 255, 0))
+                    elif test == 'blue':
+                        block(i*dotS, z*dotS, dotS, dotS, (0, 0, 255))
+                    elif test == 'purple':
+                        block(i*dotS, z*dotS, dotS, dotS, (255, 0, 130))
+
+        
+        block(x6, y6, dotS, dotS, white)
+        block(x1, y1, dotS, dotS, white)
+        block(x2, y2, dotS, dotS, white)
+        block(x3, y3, dotS, dotS, white)
+        block(x4, y4, dotS, dotS, white)
+        block(x5, y5, dotS, dotS, white)
+        
+        
         pygame.display.update()
        # clock.tick(60)
         
      
         #This part adds the various colors to a 2D List to keep track of them all.
-        block(x6, y6, dotS, dotS, (255, 0, 0))
+       # block(x6, y6, dotS, dotS, (255, 0, 0))
         try:
             arr[(int)(math.fabs(x6/dotS))].pop((int)(math.fabs(y6/dotS)))
             arr[(int)(x6/dotS)].insert(((int)(y6/dotS)), "red")
         except:
             pass
-        block(x1, y1, dotS, dotS, (255, 120, 0))
+      #  block(x1, y1, dotS, dotS, (255, 120, 0))
         try:
             arr[(int)(math.fabs(x1/dotS))].pop((int)(math.fabs(y1/dotS)))
             arr[(int)(x1/dotS)].insert(((int)(y1/dotS)), "orange")
         except:
             pass
-        block(x2, y2, dotS, dotS, (255, 255, 0))
+      #  block(x2, y2, dotS, dotS, (255, 255, 0))
         try:
             arr[(int)(math.fabs(x2/dotS))].pop((int)(math.fabs(y2/dotS)))
             arr[(int)(x2/dotS)].insert(((int)(y2/dotS)), "yellow")
         except:
             pass
-        block(x3, y3, dotS, dotS, (0, 255, 0))
+      #  block(x3, y3, dotS, dotS, (0, 255, 0))
         try:
             arr[(int)(math.fabs(x3/dotS))].pop((int)(math.fabs(y3/dotS)))
             arr[(int)(x3/dotS)].insert(((int)(y3/dotS)), "green")
         except:
             pass
-        block(x4, y4, dotS, dotS, (0, 0, 255))
+      #  block(x4, y4, dotS, dotS, (0, 0, 255))
         try:
             arr[(int)(math.fabs(x4/dotS))].pop((int)(math.fabs(y4/dotS)))
             arr[(int)(x4/dotS)].insert(((int)(y4/dotS)), "blue")
         except:
            pass
-        block(x5, y5, dotS, dotS, (255, 0, 130))
+      #  block(x5, y5, dotS, dotS, (255, 0, 130))
         try:
             arr[(int)(math.fabs(x5/dotS))].pop((int)(math.fabs(y5/dotS)))
             arr[(int)(x5/dotS)].insert(((int)(y5/dotS)), "purple")
